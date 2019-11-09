16b742e0f088073c23c6e76dbee73e8a46701bcb
samuvait <sav.v@hotmail.fi>
2019-11-09 16:53:44 +0200

    Exercise 3.4

--- index.js
+++ index.js
@@ -50,5 +50,11 @@ app.get('/api/persons/:id', (request, response) => {
 
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
 const port = 3001
 app.listen(port)
