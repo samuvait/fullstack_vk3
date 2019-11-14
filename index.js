092d25db47a5b963c67b95f57528e651140e16c4
samuvait <sav.v@hotmail.fi>
2019-11-14 17:44:54 +0200

    Exercise 3.19

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get('/info', (request, response) => {
   })
 })
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
   let persons = []
   Person.find({}).then(result => {
     result.forEach(person => {
@@ -55,17 +55,14 @@ app.get('/api/persons/:id', (request, response) => {
       response.status(404).end()
     }
 
-  }).catch(error => {
-    console.log(error)
-    response.status(400).send({ error: 'malformatted id' })
-  })
+  }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
   Person.findByIdAndRemove(request.params.id).then(response.status(204).end()).catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   console.log('body', body)
@@ -81,10 +78,11 @@ app.post('/api/persons', (request, response) => {
 
   person.save().then(savedPerson => {
     response.json(savedPerson.toJSON())
-  })
+  }).catch(error => next(error))
+
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
   const id = request.params.id
 
@@ -110,6 +108,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError' && error.kind == 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
@@ -12,8 +13,16 @@ mongoose.connect(url, { useNewUrlParser: true }).then(result => {
 })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+    unique: true
+  },
+  number: {
+    type: String,
+    required: true
+  }
 })
 
 personSchema.set('toJSON', {
@@ -23,5 +32,7 @@ personSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
+
+personSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('Person', personSchema)
